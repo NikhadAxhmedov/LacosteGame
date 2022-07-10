a1c84e7a5fed6ceb13657416e8573540ce9bba8e
Nixhad Axhmedov <108715556+NikhadAxhmedov@users.noreply.github.com>
2022-07-10 20:47:27 +0300

    Update __init__.py

--- kelime_bot/__init__.py
+++ kelime_bot/__init__.py
@@ -17,7 +17,7 @@ LOGGER = logging.getLogger(__name__)
 # Hesap
 API_ID = "17032930"
 API_HASH = "eb80de1a38db0d06656478bbeff23aee"
-TOKEN = "5449898899:AAHYZUc-lsObP5729b57myxzKDBIOfk43yI" 
+TOKEN = "5449898899:AAHC52obWaCjQtMFXJU2ouQZqWWlg1Iu7I8" 
 USERNAME = "LacosteGameAzBot"
 
 
